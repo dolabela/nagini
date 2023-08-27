3e35a4986b1ce6820c190c00f6d01a8fd0f794ed
dolabela <brenodolabela@gmail.com>
2023-08-27 14:06:16 -0300

    feat: add recall and precision test function

--- tests/performance/test_classification.py
+++ tests/performance/test_classification.py
@@ -5,5 +5,17 @@ import numpy as np
 def test_accuracy_score():
     y_true = np.array([0, 1, 0, 1])
     y_pred = np.array([0, 1, 1, 1])
-    accuracy_score = calculate_accuracy_score(y_true, y_pred)
-    assert accuracy_score == 0.75
+    actual_score = accuracy_score(y_true, y_pred)
+    assert actual_score == 0.75
+
+def test_precision_score():
+    y_true = np.array([0, 1, 0, 1])
+    y_pred = np.array([0, 1, 1, 1])
+    actual_score = precision_score(y_true, y_pred)
+    assert round(actual_score, 2) == 0.67
+
+def test_recall_score():
+    y_true = np.array([0, 1, 0, 1])
+    y_pred = np.array([0, 1, 1, 0])
+    actual_score = recall_score(y_true, y_pred)
+    assert round(actual_score, 2) == 0.5
