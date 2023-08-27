b96f9211f3e3f37652e1ab386f6208008b758f8c
dolabela <brenodolabela@gmail.com>
2023-08-27 14:14:33 -0300

    feat: add f1 test function

--- tests/performance/test_classification.py
+++ tests/performance/test_classification.py
@@ -19,3 +19,9 @@ def test_recall_score():
     y_pred = np.array([0, 1, 1, 0])
     actual_score = recall_score(y_true, y_pred)
     assert round(actual_score, 2) == 0.5
+
+def test_f1_score():
+    y_true = np.array([0, 1, 0, 1])
+    y_pred = np.array([1, 1, 1, 0])
+    actual_score = f1_score(y_true, y_pred)
+    assert round(actual_score, 2) == 0.4
